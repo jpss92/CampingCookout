95766a6bc33c1a0c84a08bf19419af59c254e65f
Justin Swallow <132158985+jpss92@users.noreply.github.com>
2023-11-09 07:34:58 -0800

    first commit

---
+++ Client/components/DocData/DocData.js
@@ -0,0 +1,19 @@
+    // import { useState } from "react";
+
+    // const DocData = () => {
+    //     const [file, setFile] = useState(null);
+
+    //     const handleFileChange = (e) => {
+    //         const selectedFile = e.target.files[0];
+    //         setFile(selectedFile);
+    //     };
+
+    //     const handleUpload = async () => {
+    //         const formData = new FormData();
+    //         formData.append('pdfFile', file);
+
+    //         try {
+    //             const response = await fetch('/camping_cookout/')
+    //         }
+    //     }
+    // }
